7358a0b4065c5782e4d29d073b321c2fa83a0088
devmfundo <devmfundo@gmail.com>
2024-09-08 23:18:22 +0200

    API suucessfully fetched and is sent to the browser, no style no user input  yet

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,43 @@
-const Hero=()=>{
-    return(
-        <hero></hero>
-    )
+import {useEffect, useState} from 'react';
+const Hero =()=>{
+
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+
+useEffect(()=>{
+    const getData=()=>{
+      fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/johannesburg?unitGroup=metric&key=VF4BL7LGST9AAZJ6JBPE4GHGH&contentType=json",{
+        mode: "cors"
+      })
+      .then(response=>response.json())
+      .then(data=>{
+        setWeatherData(data);
+      })
+      .then(error=>{
+        setError(error);
+        console.log(error)
+      });
+    }
+    getData();
+},[])
+  
+  return(
+    <div>
+        {error && <p>Error: {error.message}</p>}
+        {
+        weatherData?(
+          <nav>
+          <h2>City: {weatherData.address}</h2>
+          <h4>Temperature: {weatherData.currentConditions.temp}°C</h4>
+          <h5>Conditions: {weatherData.currentConditions.conditions}</h5>
+          </nav>
+          ) : (
+            <p>Loading</p>
+          )
+        }
+    </div>
+    
+  )
 }
+
 export default Hero;
